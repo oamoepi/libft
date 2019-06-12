692f932c9856927776b5b17228595b1ecd345c15
abnar MOEPI <amoepi@c3r9s4.wethinkcode.co.za>
2019-06-12 17:01:01 +0200

    libft.h update with struct

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: amoepi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 15:07:02 by amoepi            #+#    #+#             */
-/*   Updated: 2019/06/06 18:06:39 by amoepi           ###   ########.fr       */
+/*   Updated: 2019/06/12 16:46:57 by amoepi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,17 @@
 # include <string.h>
 # include <unistd.h>
 # include <stdlib.h>
+typedef struct	s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					*t_list;
 
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
-void	*ft_memccpy(void *dst, const void *src, int c,  size_t n);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
@@ -29,7 +35,7 @@ char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strncpy(char *s1, const char *s2, size_t len);
 char	*ft_strcat(char *restrict s1, const char *restrict s2);
 char	*ft_strncat(char *restrict s1, const char *restricts2, size_t n);
-size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *s1, const char *s2);
