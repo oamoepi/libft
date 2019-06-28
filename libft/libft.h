c7bb6742389a25be647475313405670c70503fc3
abnar MOEPI <amoepi@c3r9s4.wethinkcode.co.za>
2019-06-28 03:47:46 +0200

    complete finale

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: amoepi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 15:07:02 by amoepi            #+#    #+#             */
-/*   Updated: 2019/06/27 12:50:55 by amoepi           ###   ########.fr       */
+/*   Updated: 2019/06/28 03:41:09 by amoepi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,5 +80,4 @@ void			ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstadd(t_list **alst, t_list *new);
 void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
-t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 #endif
